d8059fe02ff47cb52ada73b355c0195c9fbb40f6
Parkjyun <jaeyun0507@naver.com>
2024-02-05 22:54:35 +0900

    [init] initial project settings

---
+++ src/main/java/com/newsnack/www/newsnackserver/NewsnackserverApplication.java
@@ -0,0 +1,13 @@
+package com.newsnack.www.newsnackserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsnackserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsnackserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newsnack/www/newsnackserver/NewsnackserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsnack.www.newsnackserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsnackserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
