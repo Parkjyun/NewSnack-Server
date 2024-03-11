d8f6e88cd6196152135a9f31843b524e187e41cb
Parkjyun <jaeyun0507@naver.com>
2024-03-11 22:58:15 +0900

    [fix] fix member entity

--- src/main/java/com/newsnack/www/newsnackserver/domain/member/model/Member.java
+++ src/main/java/com/newsnack/www/newsnackserver/domain/member/model/Member.java
@@ -2,8 +2,11 @@ package com.newsnack.www.newsnackserver.domain.member.model;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -14,8 +17,40 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    private String serialId;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private MemberRole role;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Provider provider;
+
+    private String refreshToken;
+
+    private String email;
+
     private String name;
 
     @Enumerated(EnumType.STRING)
-    private MemberStatus status;
+    private MemberStatus memberStatus;
+
+    private LocalDateTime deletedAt;
+
+    @Builder
+    public Member(String serialId, String name, String email, Provider provider, MemberRole role, String refreshToken, MemberStatus memberStatus) {
+        this.serialId = serialId;
+        this.name = name;
+        this.email = email;
+        this.provider = provider;
+        this.role = role;
+        this.refreshToken = refreshToken;
+        this.memberStatus = memberStatus;
+    }
+
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
 }
