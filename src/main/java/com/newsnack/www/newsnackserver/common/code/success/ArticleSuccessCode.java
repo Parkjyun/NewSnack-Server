740e2660a0bcfa1697aa6c995cd35adc4287770c
Parkjyun <jaeyun0507@naver.com>
2024-03-16 22:38:28 +0900

    [feat] add article success code

--- src/main/java/com/newsnack/www/newsnackserver/common/code/success/ArticleSuccessCode.java
+++ src/main/java/com/newsnack/www/newsnackserver/common/code/success/ArticleSuccessCode.java
@@ -11,7 +11,15 @@ public enum ArticleSuccessCode implements SuccessCode{
     /**
      * 200 OK
      **/
-    GET_ARTICLES_SUCCESS(HttpStatus.OK, "기사 조회에 성공했습니다");
+    GET_ARTICLES_SUCCESS(HttpStatus.OK, "기사 조회에 성공했습니다"),
+    /**
+     * 201 Created
+     **/
+    ARTICLE_LIKE_SUCCESS(HttpStatus.OK, "기사 좋아요 성공"),
+    /**
+     * 204 No Content
+     **/
+    ARTICLE_LIKE_CANCEL_SUCCESS(HttpStatus.OK, "기사 좋아요 취소 성공");
 
     private final HttpStatus httpStatus;
     private final String message;
