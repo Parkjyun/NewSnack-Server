4986786a5f493320b1a11459403028d8efdf3421
Parkjyun <jaeyun0507@naver.com>
2024-03-16 22:39:12 +0900

    [feat] create custom article exception

--- src/main/java/com/newsnack/www/newsnackserver/common/exception/ArticleException.java
+++ src/main/java/com/newsnack/www/newsnackserver/common/exception/ArticleException.java
@@ -1,13 +1,15 @@
 package com.newsnack.www.newsnackserver.common.exception;
 
 import com.newsnack.www.newsnackserver.common.code.failure.FailureCode;
+import lombok.Getter;
 
+@Getter
 public class ArticleException extends RuntimeException{
 
     private final FailureCode failureCode;
 
     public ArticleException(FailureCode failureCode) {
-        super("[AuthException] : " + failureCode.getMessage());
+        super("[ArticleException] : " + failureCode.getMessage());
         this.failureCode = failureCode;
     }
 
