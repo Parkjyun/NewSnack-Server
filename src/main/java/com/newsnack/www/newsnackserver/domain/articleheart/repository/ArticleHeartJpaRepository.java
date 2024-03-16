710c6e905252297e565cd8bd16116ef9751fd8bd
Parkjyun <jaeyun0507@naver.com>
2024-03-16 23:06:02 +0900

    [feat] create logic for article like and cancel api

--- src/main/java/com/newsnack/www/newsnackserver/domain/articleheart/repository/ArticleHeartJpaRepository.java
+++ src/main/java/com/newsnack/www/newsnackserver/domain/articleheart/repository/ArticleHeartJpaRepository.java
@@ -1,8 +1,13 @@
 package com.newsnack.www.newsnackserver.domain.articleheart.repository;
 
+import com.newsnack.www.newsnackserver.domain.article.model.Article;
 import com.newsnack.www.newsnackserver.domain.articleheart.model.ArticleHeart;
+import com.newsnack.www.newsnackserver.domain.member.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface ArticleHeartJpaRepository extends JpaRepository<ArticleHeart, Long> {
     boolean existsByArticleIdAndMemberId(Long articleId, Long memberId);
+    Optional<ArticleHeart> findByArticleAndMember(Article article, Member member);
 }
