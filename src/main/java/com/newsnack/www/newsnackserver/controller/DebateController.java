45f49778940e78e1f262f56d890d767a32830041
Parkjyun <jaeyun0507@naver.com>
2024-03-20 02:30:42 +0900

    [feat] create get debate api

--- src/main/java/com/newsnack/www/newsnackserver/controller/DebateController.java
+++ src/main/java/com/newsnack/www/newsnackserver/controller/DebateController.java
@@ -2,10 +2,12 @@ package com.newsnack.www.newsnackserver.controller;
 
 import com.newsnack.www.newsnackserver.common.code.success.DebateSuccessCode;
 import com.newsnack.www.newsnackserver.common.response.NewSnackResponse;
+import com.newsnack.www.newsnackserver.dto.response.DebateIndividualResponse;
 import com.newsnack.www.newsnackserver.dto.response.DebateMainPageResponse;
 import com.newsnack.www.newsnackserver.service.DebateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,10 @@ public class DebateController {
     @GetMapping("/main")
     public NewSnackResponse<DebateMainPageResponse> getMainDebate() {
         return NewSnackResponse.success(DebateSuccessCode.GET_DEBATES_SUCCESS, debateService.getMainDebate());
+    }
+
+    @GetMapping("/{debateId}")
+    public NewSnackResponse<DebateIndividualResponse> getDebate(@PathVariable Long debateId) {
+        return NewSnackResponse.success(DebateSuccessCode.GET_DEBATES_SUCCESS, debateService.getDebate(debateId));
     }
 }
