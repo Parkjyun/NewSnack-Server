7b4e74f944e98fabbacbf84e9027581a8b772e5c
Parkjyun <jaeyun0507@naver.com>
2024-03-21 02:30:53 +0900

    [feat] create update comment of debate participation api

--- src/main/java/com/newsnack/www/newsnackserver/controller/DebateController.java
+++ src/main/java/com/newsnack/www/newsnackserver/controller/DebateController.java
@@ -1,12 +1,17 @@
 package com.newsnack.www.newsnackserver.controller;
 
 import com.newsnack.www.newsnackserver.annotation.MemberId;
+import com.newsnack.www.newsnackserver.common.code.success.DebateParticipationSuccessCode;
 import com.newsnack.www.newsnackserver.common.code.success.DebateSuccessCode;
 import com.newsnack.www.newsnackserver.common.response.NewSnackResponse;
+import com.newsnack.www.newsnackserver.controller.parameter.SearchOrder;
+import com.newsnack.www.newsnackserver.dto.request.DebateParticipationRequest;
 import com.newsnack.www.newsnackserver.dto.request.DebateVoteRequest;
 import com.newsnack.www.newsnackserver.dto.response.DebateIndividualResponse;
 import com.newsnack.www.newsnackserver.dto.response.DebateMainPageResponse;
+import com.newsnack.www.newsnackserver.service.DebateParticipationService;
 import com.newsnack.www.newsnackserver.service.DebateService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +24,7 @@ import java.util.List;
 public class DebateController {
 
     private final DebateService debateService;
+    private final DebateParticipationService debateParticipationService;
 
     @GetMapping
     public NewSnackResponse<List<DebateMainPageResponse>> getDebates() {
@@ -39,5 +45,11 @@ public class DebateController {
     public NewSnackResponse<?> voteDebate(@PathVariable Long debateId, @MemberId Long memberId, @RequestBody DebateVoteRequest debateVoteRequest) {
         debateService.voteDebate(debateId, memberId, debateVoteRequest.vote());
         return NewSnackResponse.success(DebateSuccessCode.DEBATE_VOTE_SUCCESS);
+    }
+
+    @PostMapping("/{debateId}/comments")
+    public NewSnackResponse<?> createDebateComment(@PathVariable Long debateId, @MemberId Long memberId, @RequestBody @Valid DebateParticipationRequest request) {
+        debateParticipationService.participateDebate(debateId, memberId, request.content());
+        return NewSnackResponse.success(DebateParticipationSuccessCode.DEBATE_PARTICIPATION_SUCCESS);
     }
 }
