21d5b312014eaf2680f3aacbfc1c36104f17ecf7
Parkjyun <jaeyun0507@naver.com>
2024-03-20 18:49:31 +0900

    [fix] change logic to include field vote in get individual debate api

--- src/main/java/com/newsnack/www/newsnackserver/service/DebateService.java
+++ src/main/java/com/newsnack/www/newsnackserver/service/DebateService.java
@@ -4,6 +4,8 @@ import com.newsnack.www.newsnackserver.common.code.failure.DebateFailureCode;
 import com.newsnack.www.newsnackserver.common.exception.DebateException;
 import com.newsnack.www.newsnackserver.domain.debate.model.Debate;
 import com.newsnack.www.newsnackserver.domain.debate.repository.DebateJpaRepository;
+import com.newsnack.www.newsnackserver.domain.debateparticipation.model.DebateParticipation;
+import com.newsnack.www.newsnackserver.domain.debateparticipation.repository.DebateParticipationJpaRepository;
 import com.newsnack.www.newsnackserver.dto.response.DebateIndividualResponse;
 import com.newsnack.www.newsnackserver.dto.response.DebateMainPageResponse;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,15 +21,25 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class DebateService {
     private final DebateJpaRepository debateJpaRepository;
+    private final DebateParticipationJpaRepository debateParticipationJpaRepository;
 
     public DebateMainPageResponse getMainDebate() {
         Debate debate = debateJpaRepository.findLatestDebateWithArticleJPQL().orElseThrow(() -> new DebateException(DebateFailureCode.DEBATE_NOT_FOUND));
         return DebateMainPageResponse.from(debate);
     }
 
-    public DebateIndividualResponse getDebate(Long debateId) {
+    public DebateIndividualResponse getDebate(Long debateId, Long memberId) {//todo: 상세조회에서 투표여부 보내야함
         Debate debate = debateJpaRepository.findDebateWithArticleJPQL(debateId).orElseThrow(() -> new DebateException(DebateFailureCode.DEBATE_NOT_FOUND));
-        return DebateIndividualResponse.from(debate);
+        if (memberId == null) {//access 없다면
+            return DebateIndividualResponse.of(debate, null);
+        }
+        DebateParticipation debateParticipation;
+        try {
+            debateParticipation = debateParticipationJpaRepository.findByDebateIdAndMemberId(debateId, memberId).get();
+        } catch (NoSuchElementException e) {
+            return DebateIndividualResponse.of(debate, null);
+        }
+        return DebateIndividualResponse.of(debate, debateParticipation.getVote());
     }
 
     public List<DebateMainPageResponse> getDebates() {
