ab2de08ba0858f330cb8f0a51870021b851ad7ca
Parkjyun <jaeyun0507@naver.com>
2024-03-22 01:28:57 +0900

    [feat] add permitted path

--- src/main/java/com/newsnack/www/newsnackserver/security/config/SecurityConfig.java
+++ src/main/java/com/newsnack/www/newsnackserver/security/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
                                 .requestMatchers("/v1/articles/{articleId}", "/v1/articles", "/v1/articles/main").permitAll()
                                 .requestMatchers("v1/debates/{debateId}", "/v1/debates", "/v1/debates/main").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/v1/articles/{articleId}/comments").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/v1/debates/{debateId}/comments").permitAll()
                                 .anyRequest().authenticated())//인증 필요
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
