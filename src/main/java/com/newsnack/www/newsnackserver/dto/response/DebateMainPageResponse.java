d3fb3de4a664803db32ea1c41f52bed1fe8957e3
Parkjyun <jaeyun0507@naver.com>
2024-03-26 01:48:28 +0900

    [fix] fix responsedto's field type LocalDateTime to String

--- src/main/java/com/newsnack/www/newsnackserver/dto/response/DebateMainPageResponse.java
+++ src/main/java/com/newsnack/www/newsnackserver/dto/response/DebateMainPageResponse.java
@@ -6,11 +6,11 @@ import com.newsnack.www.newsnackserver.domain.debate.model.Debate;
 import java.time.LocalDateTime;
 
 public record DebateMainPageResponse(long debateId, String title, int agreeCount, int disagreeCount, long articleId, String imageUrl,
-                                     String articleTitle, SectionCategory sectionCategory, LocalDateTime articleCreatedAt) {
+                                     String articleTitle, SectionCategory sectionCategory, String articleCreatedAt) {
 
     public static DebateMainPageResponse from(Debate debate) {
         return new DebateMainPageResponse(debate.getId(), debate.getTitle(), debate.getUpVoteCount(), debate.getDownVoteCount(),
                 debate.getArticle().getId(), debate.getArticle().getImageUrl(), debate.getArticle().getTitle(), debate.getArticle().getSectionCategory(),
-                debate.getArticle().getCreatedAt());
+                debate.getArticle().getCreatedAt().toString());
     }
 }
